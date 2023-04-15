4f783bfd0202df2a44c722a94534042d3c2c5460
Olga Komar <komar_ol@mail.ru>
2023-04-15 17:21:40 +0300

    added he solution to the first task

--- Program.cs
+++ Program.cs
@@ -5,4 +5,17 @@
 //782 -> 8
 //918 -> 1
 
+int NumberEntry()
+{
+    Console.Write("Введите трехзначное число= ");
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+void SecondDigit(int number){
+    number = number/10;
+    number = number%10;
+    Console.WriteLine($"Вторая цифра = {number}");
+}
+
+int num = NumberEntry();
+SecondDigit(num);
